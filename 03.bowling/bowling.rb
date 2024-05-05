db2eed7da23fa42a8907ca61a1abc91a891b84e9
fukuroq <fukurou.bbb.23@gmail.com>
2024-05-05 22:03:20 +0900

    ガード節の後に空行を追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -21,6 +21,7 @@ end
 point = 0
 frames.each_with_index do |frame, i|
   break if i > 9 # 10フレーム分(iが0~9)の計算を終えたら処理を終える
+
   if frame[0] == 10 # ストライク
     if frames[i + 1][0] == 10 # 次の投球もストライクだった場合
       point += frame[0] + frames[i + 1][0] + frames[i + 2][0]
