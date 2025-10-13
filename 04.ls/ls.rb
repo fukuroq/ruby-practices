70dea89e2b5a5e1aa9fdf223169f7ef4f492f484
fukuroq <fukurou.bbb.23@gmail.com>
2025-10-14 01:12:28 +0900

    lオプションを受け取りブロック数の合計値を表示する処理を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,12 +6,33 @@ MAX_NUMBER_OF_COLUMNS = 3
 TAB_WIDTH = 8 # ターミナルのデフォルトのタブの文字数
 
 def main
-  options = ARGV.getopts('ar')
+  options = ARGV.getopts('arl')
   file_names = Dir.glob('*', options['a'] ? File::FNM_DOTMATCH : 0)
   sorted_file_names = options['r'] ? file_names.reverse : file_names
-  number_of_rows = sorted_file_names.length.ceildiv(MAX_NUMBER_OF_COLUMNS)
-  column_width = calcurate_column_width(sorted_file_names)
-  output_file_names(sorted_file_names, number_of_rows, column_width)
+  if options['l']
+    long_format_file_names = generate_long_format(sorted_file_names)
+    total_block_size = long_format_file_names.inject(0) { |sum, hash| sum + hash[:block_size] }
+    output_long_format(long_format_file_names, total_block_size)
+  else
+    number_of_rows = sorted_file_names.length.ceildiv(MAX_NUMBER_OF_COLUMNS)
+    column_width = calcurate_column_width(sorted_file_names)
+    output_file_names(sorted_file_names, number_of_rows, column_width)
+  end
+end
+
+def generate_long_format(file_names)
+  long_format_file_names = []
+  file_names.each do |file_name|
+    file_status = File::Stat.new(file_name)
+    long_format_file_names << {
+      block_size: file_status.blocks
+    }
+  end
+  long_format_file_names
+end
+
+def output_long_format(long_format_file_names, total_block_size)
+  puts "total: #{total_block_size}"
 end
 
 def calcurate_column_width(file_names)
