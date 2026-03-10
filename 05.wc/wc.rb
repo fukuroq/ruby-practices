b2ec8bc26b86d161cd5d12a0a6dd6ada1bc0fd5d
fukuroq <fukurou.bbb.23@gmail.com>
2026-03-10 19:22:17 +0900

    lオプション、wオプション、cオプションの処理を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 TAB_WIDTH = 8 # ターミナルのデフォルトのタブの文字数
 
 def main
+  options = ARGV.getopts('lwc')
+  show_all = options.values.none?
   file_name = ARGV[0]
   file_text = File.read(file_name)
   result = []
-  result << format("% #{TAB_WIDTH}d", file_text.count("\n"))
-  result << format("% #{TAB_WIDTH}d", file_text.split.count)
-  result << format("% #{TAB_WIDTH}d", file_text.bytesize)
+  result << format("% #{TAB_WIDTH}d", file_text.count("\n")) if show_all || options['l']
+  result << format("% #{TAB_WIDTH}d", file_text.split.count) if show_all || options['w']
+  result << format("% #{TAB_WIDTH}d", file_text.bytesize) if show_all || options['c']
   result << " #{file_name}"
   puts result.join
 end
