b4ea9e9a0c8435e8a1df109a5dba26519b7e7579
fukuroq <fukurou.bbb.23@gmail.com>
2026-03-10 22:27:49 +0900

    build_file_statistics_total()を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,14 +8,7 @@ def main
   options = ARGV.getopts('lwc')
   file_names = ARGV
   file_statistics = build_file_statistics(file_names)
-  if file_statistics.size > 1
-    file_statistics << {
-      line_count: file_statistics.sum { it[:line_count] },
-      word_count: file_statistics.sum { it[:word_count] },
-      byte_count: file_statistics.sum { it[:byte_count] },
-      file_name: 'total'
-    }
-  end
+  file_statistics << build_file_statistics_total(file_statistics) if file_statistics.size > 1
   formated_rows = format_rows(file_statistics, options)
   puts formated_rows.join("\n")
 end
@@ -30,6 +23,15 @@ def build_file_statistics(file_names)
       file_name: file_name
     }
   end
+end
+
+def build_file_statistics_total(file_statistics)
+  {
+    line_count: file_statistics.sum { it[:line_count] },
+    word_count: file_statistics.sum { it[:word_count] },
+    byte_count: file_statistics.sum { it[:byte_count] },
+    file_name: 'total'
+  }
 end
 
 def format_rows(file_statistics, options)
