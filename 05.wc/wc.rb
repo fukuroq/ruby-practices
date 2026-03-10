ad73903157988c91898f4a061481fefae90b7b10
fukuroq <fukurou.bbb.23@gmail.com>
2026-03-10 22:23:16 +0900

    format_rows()を追加

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,26 +6,18 @@ TAB_WIDTH = 8 # ターミナルのデフォルトのタブの文字数
 
 def main
   options = ARGV.getopts('lwc')
-  show_all = options.values.none?
   file_names = ARGV
   file_statistics = build_file_statistics(file_names)
-  file_statistics.each do |file_statistic|
-    result = []
-    result << format("% #{TAB_WIDTH}d", file_statistic[:line_count]) if show_all || options['l']
-    result << format("% #{TAB_WIDTH}d", file_statistic[:word_count]) if show_all || options['w']
-    result << format("% #{TAB_WIDTH}d", file_statistic[:byte_count]) if show_all || options['c']
-    result << " #{file_statistic[:file_name]}"
-    puts result.join
-  end
-
   if file_statistics.size > 1
-    result = []
-    result << format("% #{TAB_WIDTH}d", file_statistics.sum { it[:line_count] }) if show_all || options['l']
-    result << format("% #{TAB_WIDTH}d", file_statistics.sum { it[:word_count] }) if show_all || options['w']
-    result << format("% #{TAB_WIDTH}d", file_statistics.sum { it[:byte_count] }) if show_all || options['c']
-    result << " total"
-    puts result.join
+    file_statistics << {
+      line_count: file_statistics.sum { it[:line_count] },
+      word_count: file_statistics.sum { it[:word_count] },
+      byte_count: file_statistics.sum { it[:byte_count] },
+      file_name: 'total'
+    }
   end
+  formated_rows = format_rows(file_statistics, options)
+  puts formated_rows.join("\n")
 end
 
 def build_file_statistics(file_names)
@@ -37,6 +29,18 @@ def build_file_statistics(file_names)
       byte_count: file_text.bytesize,
       file_name: file_name
     }
+  end
+end
+
+def format_rows(file_statistics, options)
+  show_all = options.values.none?
+  file_statistics.map do |file_statistic|
+    result = []
+    result << format("% #{TAB_WIDTH}d", file_statistic[:line_count]) if show_all || options['l']
+    result << format("% #{TAB_WIDTH}d", file_statistic[:word_count]) if show_all || options['w']
+    result << format("% #{TAB_WIDTH}d", file_statistic[:byte_count]) if show_all || options['c']
+    result << " #{file_statistic[:file_name]}"
+    result.join
   end
 end
 
