2bea2b5da4ce81873108260188606a0ee69f128b
fukuroq <fukurou.bbb.23@gmail.com>
2026-07-14 18:18:39 +0900

    オプションで指定された値のみをtext_statsに格納するように変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,45 +9,38 @@ def main
   file_names = ARGV
   text_stats =
     if file_names.empty?
-      [build_text_stat($stdin.read)]
+      [build_text_stat($stdin.read, options)]
     else
-      file_names.map { build_text_stat(File.read(it), it) }
+      file_names.map { build_text_stat(File.read(it), options, it) }
     end
   text_stats << build_text_stats_total(text_stats) if text_stats.size > 1
-  columns = collect_show_count_columns(options)
-  puts format_rows(text_stats, columns)
+  puts format_rows(text_stats)
 end
 
-def build_text_stat(text, file_name = '')
+def build_text_stat(text, options, file_name = '')
+  show_all = options.values.none?
+  counts = {}
+  counts[:line_count] = text.count("\n") if show_all || options[:l]
+  counts[:word_count] = text.split.count if show_all || options[:w]
+  counts[:byte_count] = text.bytesize if show_all || options[:c]
   {
-    line_count: text.count("\n"),
-    word_count: text.split.count,
-    byte_count: text.bytesize,
+    counts: counts,
     file_name:
   }
 end
 
 def build_text_stats_total(text_stats)
+  total_counts = Hash.new(0)
+  text_stats.each { it[:counts].each { |key, value| total_counts[key] += value } }
   {
-    line_count: text_stats.sum { it[:line_count] },
-    word_count: text_stats.sum { it[:word_count] },
-    byte_count: text_stats.sum { it[:byte_count] },
+    counts: total_counts,
     file_name: 'total'
   }
 end
 
-def collect_show_count_columns(options)
-  show_all = options.values.none?
-  columns = []
-  columns << :line_count if show_all || options[:l]
-  columns << :word_count if show_all || options[:w]
-  columns << :byte_count if show_all || options[:c]
-  columns
-end
-
-def format_rows(text_stats, columns)
+def format_rows(text_stats)
   text_stats.map do |text_stat|
-    counts_part = columns.map { format("% #{TAB_WIDTH}d", text_stat[it]) }.join
+    counts_part = text_stat[:counts].map { |_key, value| format("% #{TAB_WIDTH}d", value) }.join
     file_name_part = text_stat[:file_name].empty? ? '' : " #{text_stat[:file_name]}"
     "#{counts_part}#{file_name_part}"
   end
