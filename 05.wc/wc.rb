4e9b701b39e9a875137f9b57356aeb5839f99d66
fukuroq <fukurou.bbb.23@gmail.com>
2026-07-14 16:38:17 +0900

    テキストの情報を格納する変数の名前をtext_statisticからtext_statに変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -7,18 +7,18 @@ TAB_WIDTH = 8 # ターミナルのデフォルトのタブの文字数
 def main
   options = ARGV.getopts('lwc')
   file_names = ARGV
-  text_statistics =
+  text_stats =
     if file_names.empty?
-      [build_text_statistic($stdin.read)]
+      [build_text_stat($stdin.read)]
     else
-      file_names.map { build_text_statistic(File.read(it), it) }
+      file_names.map { build_text_stat(File.read(it), it) }
     end
-  text_statistics << build_text_statistics_total(text_statistics) if text_statistics.size > 1
+  text_stats << build_text_stats_total(text_stats) if text_stats.size > 1
   columns = collect_show_count_columns(options)
-  puts format_rows(text_statistics, columns)
+  puts format_rows(text_stats, columns)
 end
 
-def build_text_statistic(text, file_name = '')
+def build_text_stat(text, file_name = '')
   {
     line_count: text.count("\n"),
     word_count: text.split.count,
@@ -27,11 +27,11 @@ def build_text_statistic(text, file_name = '')
   }
 end
 
-def build_text_statistics_total(text_statistics)
+def build_text_stats_total(text_stats)
   {
-    line_count: text_statistics.sum { it[:line_count] },
-    word_count: text_statistics.sum { it[:word_count] },
-    byte_count: text_statistics.sum { it[:byte_count] },
+    line_count: text_stats.sum { it[:line_count] },
+    word_count: text_stats.sum { it[:word_count] },
+    byte_count: text_stats.sum { it[:byte_count] },
     file_name: 'total'
   }
 end
@@ -45,10 +45,10 @@ def collect_show_count_columns(options)
   columns
 end
 
-def format_rows(text_statistics, columns)
-  text_statistics.map do |text_statistic|
-    counts_part = columns.map { format("% #{TAB_WIDTH}d", text_statistic[it]) }.join
-    file_name_part = text_statistic[:file_name].empty? ? '' : " #{text_statistic[:file_name]}"
+def format_rows(text_stats, columns)
+  text_stats.map do |text_stat|
+    counts_part = columns.map { format("% #{TAB_WIDTH}d", text_stat[it]) }.join
+    file_name_part = text_stat[:file_name].empty? ? '' : " #{text_stat[:file_name]}"
     "#{counts_part}#{file_name_part}"
   end
 end
