cd6820e49cbd6b467cb39a2d6d864d52d4f36da2
fukuroq <fukurou.bbb.23@gmail.com>
2026-07-15 22:50:15 +0900

    outputメソッドを作成

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -13,10 +13,7 @@ def main
     else
       file_names.map { build_text_stat(File.read(it), options, it) }
     end
-  text_stats.each do |text_stat|
-    puts format_row(text_stat[:counts], text_stat[:file_name])
-  end
-  puts format_row(build_total_counts(text_stats), 'total') if text_stats.size > 1
+  output(text_stats, text_stats.size > 1)
 end
 
 def build_text_stat(text, options, file_name = '')
@@ -25,22 +22,24 @@ def build_text_stat(text, options, file_name = '')
   counts[:line_count] = text.count("\n") if show_all || options[:l]
   counts[:word_count] = text.split.count if show_all || options[:w]
   counts[:byte_count] = text.bytesize if show_all || options[:c]
-  {
-    counts: counts,
-    file_name:
-  }
+  { counts: counts, file_name: file_name }
 end
 
-def build_total_counts(text_stats)
-  total_counts = Hash.new(0)
-  text_stats.each { it[:counts].each { |key, value| total_counts[key] += value } }
-  total_counts
+def output(text_stats, show_total)
+  text_stats.each { puts format_row(it[:counts], it[:file_name]) }
+  puts format_row(build_total_counts(text_stats), 'total') if show_total
 end
 
 def format_row(counts, file_name)
   counts_part = counts.map { |_key, value| format("% #{TAB_WIDTH}d", value) }.join
   file_name_part = file_name.empty? ? '' : " #{file_name}"
   "#{counts_part}#{file_name_part}"
+end
+
+def build_total_counts(text_stats)
+  total_counts = Hash.new(0)
+  text_stats.each { it[:counts].each { |key, value| total_counts[key] += value } }
+  total_counts
 end
 
 main
